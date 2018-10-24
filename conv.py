aa70e72358b33b64b743e0b28d51538f7ee38c64
Orazio <sciuto.orazio@studium.unict.it>
2018-10-24 20:11:41 +0200

    strutta ok

--- conv.py
+++ conv.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from telegram.ext import Updater, CommandHandler, Job, CallbackQueryHandler
+from telegram.ext import Updater, CommandHandler, Job, CallbackQueryHandler, MessageHandler, Filters
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 import logging
 import requests
@@ -17,6 +17,7 @@ logger = logging.getLogger(__name__)
 # Define a few command handlers. These usually take the two arguments bot and
 # update. Error handlers also receive the raised TelegramError object in error.
 def start(bot, update):
+    print("sono partito")
     update.message.reply_text('Ciao! Mi occupo di convertire numeri tra basi diverse')
 
 
@@ -24,16 +25,36 @@ def error(bot, update, error):
     logger.warning('Update "%s" caused error "%s"' % (update, error))
 
 
-def bintodec(bot, update, args):
-    t=args[0];
-    try:
-        a=int(t,2)
-        update.message.reply_text(a)
-    except ValueError as verr:
-       update.message.reply_text('Ma cosa hai inserito?')
-    except Exception as ex:
-        update.message.reply_text('Errore!')
+def converto(bot, update, num, p, a):
+    txt= "Converto " + num + " dalla base " + p + " alla base " + a
+    print(txt)
+    update.message.reply_text(txt)
+    if(p=='2'):
+        try:
+            newdec=int(num,2)
+            #newoct=oct(num)
+            newhex=hex(num,2)
+            if(a==2): 
+                update.message.reply_text("Risultato: " + num)
+            #if(a=='8'): 
+                #update.message.reply_text("Risultato: " + newoct)
+            if(a=='10'): 
+                update.message.reply_text("Risultato: " + newdec)
+            if(a==16): 
+                update.message.reply_text("Risultato: " + newhex)
+        except ValueError as verr:
+            update.message.reply_text('Controlla di aver inserito un numero binario')
+        
 
+
+def echo(bot, update):
+    splitted = update.message.text.split()
+    print(len(splitted))
+    if(len(splitted)==3):
+        pa = splitted[0]
+        num = splitted[1]
+        de = splitted[2]
+        converto(bot, update, num,pa,de)
 
 
 
@@ -42,10 +63,12 @@ def main():
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
+
+
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", start))
-    dp.add_handler(CommandHandler("2to10", bintodec, pass_args=True))
+    dp.add_handler(MessageHandler(Filters.text, echo))
 
     # log all errors
     dp.add_error_handler(error)
